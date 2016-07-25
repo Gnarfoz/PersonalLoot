b56eabcf54a4a15ccb593ff93de83295bfd7f4bc
Steven Jackson <sj@oscode.net>
2016-07-25 14:40:49 +0100

    Fix trace case

--- PersonalLoot.lua
+++ PersonalLoot.lua
@@ -1,6 +1,6 @@
 PersonalLoot = LibStub("AceAddon-3.0"):NewAddon("PersonalLoot", "AceConsole-3.0", "AceEvent-3.0")
 
-function PersonalLoot:trace(message)
+function PersonalLoot:Trace(message)
 	if self.debugging then
 		self:Print(message)
 	end
@@ -20,21 +20,21 @@ function PersonalLoot:CHAT_MSG_LOOT(id, message)
 	end
 
 	if self:IsTradable(owner, item_link) then
-		self:trace("item is tradable")
+		self:Trace("item is tradable")
 		self:EnumerateTradees(owner, item_link)
 	end
 end
 
 function PersonalLoot:PARTY_LOOT_METHOD_CHANGED()
 	local method = GetLootMethod()
-	self:trace("PARTY_LOOT_METHOD_CHANGED: "..method)
+	self:Trace("PARTY_LOOT_METHOD_CHANGED: "..method)
 	self.is_personal_loot = method == "personalloot"
 	self:UpdateChatMsgLootRegistration()
 end
 
 function PersonalLoot:ZONE_CHANGED_NEW_AREA()
 	self.in_raid = select(2, IsInInstance()) == "raid"
-	self:trace("ZONE_CHANGED_NEW_AREA")
+	self:Trace("ZONE_CHANGED_NEW_AREA")
 	self:UpdateChatMsgLootRegistration()
 end
 
@@ -53,10 +53,10 @@ function PersonalLoot:IsTradable(owner, item)
 		return
 	end
 
-	self:trace("slot name "..slot_name)
+	self:Trace("slot name "..slot_name)
 
 	local slot_id = GetInventorySlotInfo(slot_name)
-	self:trace("slot id "..slot_id)
+	self:Trace("slot id "..slot_id)
 
 	local equipped_item = GetInventoryItemID(owner, slot_id)
 	-- TODO:
@@ -64,7 +64,7 @@ function PersonalLoot:IsTradable(owner, item)
 	-- How about 2 trinkets, 2 rings, 2 weapons, one with a higher ilvl, one with lower?
 
 	local equipped_item_level = select(4, GetItemInfo(equipped_item))
-	self:trace("equipped "..equipped_item_level..", "..level..", slot="..slot)
+	self:Trace("equipped "..equipped_item_level..", "..level..", slot="..slot)
 	return equipped_item_level > level
 end
 
@@ -73,7 +73,7 @@ function PersonalLoot:EnumerateTradees(owner, item_id)
 end
 
 function PersonalLoot:OnEnable()
-	self:trace("OnEnable")
+	self:Trace("OnEnable")
 	self.debugging = false
 	-- Reloading the UI doesn't result in these events being fired, so force them
 	self:PARTY_LOOT_METHOD_CHANGED()
@@ -83,6 +83,6 @@ function PersonalLoot:OnEnable()
 end
 
 function PersonalLoot:OnDisable()
-	self:trace("OnDisable")
+	self:Trace("OnDisable")
 	self:UnregisterAllEvents()
 end
