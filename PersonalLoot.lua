b6114c5f26fc4d8a8a1cada22121ab9613244ac4
mafernnd <Maxime.FernandezAlonso@UGent.be>
2016-07-27 01:37:58 +0200

    getRealIlvl fixed with upgradetable, titanforged not tested

--- PersonalLoot.lua
+++ PersonalLoot.lua
@@ -57,6 +57,13 @@ LibStub("AceConfig-3.0"):RegisterOptionsTable("PersonalLoot", options, "pl")
 LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("PersonalLoot", options)
 LibStub("AceConfigDialog-3.0"):AddToBlizOptions("PersonalLoot", "PersonalLoot")
 
+-- Upgrade table
+local upgradeTable = {
+	["529"] = 0,
+	["530"] = 5,
+	["531"] = 10,
+}
+
 function PersonalLoot:Trace(message)
   if self.isDebugging then
     self:Print(message)
@@ -208,6 +215,17 @@ end
 -- owner and itemLink must be valid
 function PersonalLoot:GetRealItemLevel(itemLink)
   local itemLevel = select(4, GetItemInfo(itemLink))
+  local numBonuses = select(14, strsplit(":", link, 15))
+  local affixes = select(15, strsplit(":", link, 15))
+  local upgrade = 0
+  -- loop over item bonuses in search for upgrade
+  for i = 1, numBonuses+1 do
+			local bonusID = select(i, strsplit(":", affixes))
+      if upgradeTable[bonusID] ~= nil then
+          itemLevel = itemLevel + upgradeTable[bonusID]
+      end
+  end
+  
   self:Trace(itemLink.." has item level "..itemLevel)
   return itemLevel
 end
